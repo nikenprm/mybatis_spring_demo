972f713f1ab8235374f125cac901e7c4d2611da8
Niken Pramadhantie <nikenprm@yahoo.com>
2017-07-04 14:11:26 +0700

    First commit

---
+++ src/main/java/com/niken/spring/demo/mybatis_spring_demo/MybatisSpringDemoApplication.java
@@ -0,0 +1,12 @@
+package com.niken.spring.demo.mybatis_spring_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisSpringDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MybatisSpringDemoApplication.class, args);
+	}
+}
